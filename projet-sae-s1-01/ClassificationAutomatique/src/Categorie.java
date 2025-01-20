07353d123f60818c842f4763b71626365b0a75ad
Diogo69008 <diogo69008@gmail.com>
2025-01-20 13:45:51 +0100

    Add files via upload

---
+++ projet-sae-s1-01/ClassificationAutomatique/src/Categorie.java
@@ -0,0 +1,57 @@
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Categorie {
+
+    private String nom; // le nom de la catégorie p.ex : sport, politique,...
+    private ArrayList<PaireChaineEntier> lexique; //le lexique de la catégorie
+
+    // constructeur
+    public Categorie(String nom) {
+        this.nom = nom;
+    }
+
+
+    public String getNom() {
+        return nom;
+    }
+
+
+    public  ArrayList<PaireChaineEntier> getLexique() {
+        return lexique;
+    }
+
+
+    // initialisation du lexique de la catégorie à partir du contenu d'un fichier texte
+    public void initLexique(String nomFichier) {
+        lexique = new ArrayList<>();
+        try {
+            // lecture du fichier d'entrée
+            FileInputStream file = new FileInputStream(nomFichier);
+            Scanner scanner = new Scanner(file);
+
+            while (scanner.hasNextLine()) {
+
+                String ligne = scanner.nextLine();
+                String[] subligne = ligne.split(":");
+                int entier = Integer.parseInt(subligne[1]);
+                PaireChaineEntier PaireChaine = new PaireChaineEntier(subligne[0],entier);
+                lexique.add(PaireChaine);
+                ligne = scanner.nextLine();
+            }
+            scanner.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    //calcul du score d'une dépêche pour la catégorie
+    public int score(Depeche d) {
+        return 0;
+    }
+
+
+}
---
+++ projet-sae-s1-01/ClassificationAutomatique/src/PaireChaineEntier.java
@@ -0,0 +1,24 @@
+public class PaireChaineEntier {
+    private String chaine;
+    private int entiers;
+
+    public String getChaine() {
+        return chaine;
+    }
+
+    public void setChaine(String chaine) {
+        this.chaine = chaine;
+    }
+
+    public int getEntiers() {
+        return entiers;
+    }
+
+    public void setEntiers(int entiers) {
+        this.entiers = entiers;
+    }
+    public PaireChaineEntier(String str, int ent){
+        chaine=str;
+        entiers = ent;
+    }
+}
